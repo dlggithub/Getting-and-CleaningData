bedd7185cfac54d84ff427a911d6ba603dbf0234
dlggithub <dl.grrr@yahoo.com>
2016-02-04 17:49:57 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,15 +1,10 @@
-	## Merges the training and the test sets to create one data set.
-	
-	featuretest <- read.table("~/Desktop/datacleanassig/UCI HAR Dataset/test/X_test.txt", quote="", comment.char="")
+## 1 Merges the training and the test sets to create one data set.
 
+featuretest <- read.table("~/Desktop/datacleanassig/UCI HAR Dataset/test/X_test.txt", quote="", comment.char="")
 featuretrain <- read.table("~/Desktop/datacleanassig/UCI HAR Dataset/train/X_train.txt", quote="", comment.char="")
-
 subjectest <- read.table("~/Desktop/datacleanassig/UCI HAR Dataset/test/subject_test.txt", quote="", comment.char="")
-
 subjecttrain <- read.table("~/Desktop/datacleanassig/UCI HAR Dataset/train/subject_train.txt", quote="", comment.char="")
-
 activitytest <- read.table("~/Desktop/datacleanassig/UCI HAR Dataset/test/y_test.txt", quote="", comment.char="")
-
 activitytrain <- read.table("~/Desktop/datacleanassig/UCI HAR Dataset/train/y_train.txt", quote="", comment.char="")
 
 View(featuretest)
@@ -38,7 +33,6 @@ xtest <- read.table("~/Desktop/datacleanassig/UCI HAR Dataset/test/X_test.txt", 
 View(xtest)
 str(xtest)
 
-  
 datafeatures <- read.table("~/Desktop/datacleanassig/UCI HAR Dataset/features.txt", quote="", comment.char="")
 View(datafeatures)
 names(featuredata) <- datafeatures$V2
@@ -46,28 +40,36 @@ actsubj <- cbind(activitydata, subjectdata)
 DataSet <- cbind(featuredata, actsubj)
 View(DataSet)
 
-	## Extracts only the measurements on the mean and standard deviation for each measurement.
-	
-	subdatafeatures <- datafeatures$V2[grep("mea\\(\\)|std\\(\\)", datafeatures$V2)]
+subdatafeatures <- datafeatures$V2[grep("mea\\(\\)|std\\(\\)", datafeatures$V2)]
 activitysubjectnames <- c(as.character(subdatafeatures), "activity", "subject")
 DataSet <- subset(DataSet, select = activitysubjectnames)
 str(DataSet)
 
-	## Uses descriptive activity names to name the activities in the data set
-	
-	DataSet3 <- merge(DataSet, activitynames)
+## 3 Uses descriptive activity names to name the activities in the data set
+
+DataSet3 <- merge(DataSet, activitynames)
 str(DataSet3)
+activitylabels <- read.table("~/Desktop/datacleanassig/UCI HAR Dataset/activity_labels.txt", quote="", comment.char="")
+activitycode <- "V1"
+activity <- "V2"
+View(activitylabels)
+DataSet3 <- merge(DataSet, activitylabels)
 
-	## Appropriately labels the data set with descriptive variable names.
-	
-	names(DataSet3) <- gsub("V2", "activity", names(DataSet3))
+
+## 4 Appropriately labels the data set with descriptive variable names.
+
+DataSet3 <- merge(DataSet, activitynames)
+str(DataSet3)
+activitylabels <- read.table("~/Desktop/datacleanassig/UCI HAR Dataset/activity_labels.txt", quote="", comment.char="")
+
+names(DataSet3) <- gsub("V2", "activitylabels", names(DataSet3))
 head(DataSet3, 2)
-
-names(DataSet3) <- gsub("V2", "activity", names(DataSet3))
+names(DataSet3) <- gsub("V2", "activitylabels", names(DataSet3))
 names(DataSet3) <- gsub("t", "time", names(DataSet3))
 names(DataSet3) <- gsub("f", "frequency", names(DataSet3))
 names(DataSet3) <- gsub("f", "frequency", names(DataSet3))
 head(DataSet3, 10)
-	## From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
-	
-	write.table(DataSet3, File = "tidydata.txt")
+
+##5 From the data set in step 4, creat a second independent tidy data set with the average for eact variable.
+write.table(DataSet3, file = "tidy.txt", row.name = FALSE, col.names = TRUE)
+
